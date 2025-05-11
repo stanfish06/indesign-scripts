ef02fcdb7858ed0ec80d14419ad43ca29ddf5e25
stanfish06 <93099613+stanfish06@users.noreply.github.com>
2025-05-10 21:18:33 -0400

    update subgroup layout

--- Scripts Panel/regular_grid_w_subgroups_and_labels.jsx
+++ Scripts Panel/regular_grid_w_subgroups_and_labels.jsx
@@ -1,7 +1,7 @@
-const supTitleBarHeight = 30;
-const supSideBarWidth = 110;
+const supTitleBarHeight = 0;
+const supSideBarWidth = 130;
 const titleBarHeight = 30;
-const sideBarWidth = 110;
+const sideBarWidth = 0;
 const subGridWidth = 325;
 const subGridHeight = 300;
 const margin = 10;
@@ -10,8 +10,8 @@ const subGridFrameWidth = 5;
 const subGridOffset = 5;
 var cropTop = 0; // Top crop
 var cropBottom = 0; // Bottom crop
-var cropLeft = 0; // Left crop
-var cropRight = 0; // Right crop
+var cropLeft = 100; // Left crop
+var cropRight = 100; // Right crop
 
 const textGap = 20;
 const sideBarFontSize = 20;
@@ -92,45 +92,98 @@ subGrid.prototype.setChildGrid = function (childGrid, position) {
 
 const gridList = [
   new subGrid(
-    2,
-    2,
-    "test1",
-    ["A", "B", "C", "D"],
+    3,
+    3,
+    "TFAP2C against SOX17 (colored by TBXT)",
+    ["DAPI", "TFAP2C", "TBXT", "SOX17"],
     ["lightGray", "red", "green", "blue"],
-    ["treat 1", "treat 2", "treat 3", "treat 4"],
-    "path 1",
-    "prefix1_",
-    "_postfix1.png",
+    [
+      "DEa-DEb;C2A100 (day 1);A100L250 (day 2)",
+      "APS-DEb;C4A30F20 (day 1);A100L250 (day 2)",
+      "MPS-DEb;B40C6A30F20 (day 1);A100L250 (day 2)",
+      "PPS-DEb;B40C6F20 (day 1);A100L250 (day 2)",
+      "DEa-LM;C2A100 (day 1);B30+IWP2+A8301 (day 2)",
+      "APS-LM;C4A30F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "MPS-LM;B40C6A30F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "PPS-LM;B40C6F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "PPS-DEb (mstar);B40C6F20 (day 1);A100L250 (day 2)",
+    ],
+    "C:/Users/zhiyu/OneDrive - Umich/dump/2025-05-08/scatter_AP2C_MP",
+    "SOX17_TFAP2C_",
+    "_scatter_TBXT.png",
     "matlabBlue"
   ),
   new subGrid(
-    2,
-    1,
-    "test2",
-    ["A", "B", "C", "D"],
+    3,
+    3,
+    "TFAP2C against SOX17 (colored by density)",
+    ["DAPI", "TFAP2C", "TBXT", "SOX17"],
     ["lightGray", "red", "green", "blue"],
-    ["treat 1", "treat 2", "treat 3", "treat 4"],
-    "path 2",
-    "prefix2_",
-    "_postfix2.png",
+    [
+      "DEa-DEb;C2A100 (day 1);A100L250 (day 2)",
+      "APS-DEb;C4A30F20 (day 1);A100L250 (day 2)",
+      "MPS-DEb;B40C6A30F20 (day 1);A100L250 (day 2)",
+      "PPS-DEb;B40C6F20 (day 1);A100L250 (day 2)",
+      "DEa-LM;C2A100 (day 1);B30+IWP2+A8301 (day 2)",
+      "APS-LM;C4A30F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "MPS-LM;B40C6A30F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "PPS-LM;B40C6F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "PPS-DEb (mstar);B40C6F20 (day 1);A100L250 (day 2)",
+    ],
+    "C:/Users/zhiyu/OneDrive - Umich/dump/2025-05-08/scatter_AP2C_MP",
+    "SOX17_TFAP2C_",
+    "_scatter_density.png",
+    "matlabBlue"
+  ),
+  new subGrid(
+    3,
+    3,
+    "FOXA2 against HAND1 (colored by EOMES)",
+    ["DAPI", "FOXA2", "EOMES", "HAND1"],
+    ["lightGray", "red", "green", "blue"],
+    [
+      "DEa-DEb;C2A100 (day 1);A100L250 (day 2)",
+      "APS-DEb;C4A30F20 (day 1);A100L250 (day 2)",
+      "MPS-DEb;B40C6A30F20 (day 1);A100L250 (day 2)",
+      "PPS-DEb;B40C6F20 (day 1);A100L250 (day 2)",
+      "DEa-LM;C2A100 (day 1);B30+IWP2+A8301 (day 2)",
+      "APS-LM;C4A30F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "MPS-LM;B40C6A30F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "PPS-LM;B40C6F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "PPS-DEb (mstar);B40C6F20 (day 1);A100L250 (day 2)",
+    ],
+    "C:/Users/zhiyu/OneDrive - Umich/dump/2025-05-08/scatter_FOXA2_MP",
+    "HAND1_FOXA2_",
+    "_scatter_EOMES.png",
     "matlabOrange"
   ),
   new subGrid(
-    1,
-    2,
-    "test3",
-    ["A", "B", "C", "D"],
+    3,
+    3,
+    "FOXA2 against HAND1 (colored by density)",
+    ["DAPI", "FOXA2", "EOMES", "HAND1"],
     ["lightGray", "red", "green", "blue"],
-    ["treat 1", "treat 2", "treat 3", "treat 4"],
-    "path 3",
-    "prefix3_",
-    "_postfix3.png",
-    "matlabPurple"
+    [
+      "DEa-DEb;C2A100 (day 1);A100L250 (day 2)",
+      "APS-DEb;C4A30F20 (day 1);A100L250 (day 2)",
+      "MPS-DEb;B40C6A30F20 (day 1);A100L250 (day 2)",
+      "PPS-DEb;B40C6F20 (day 1);A100L250 (day 2)",
+      "DEa-LM;C2A100 (day 1);B30+IWP2+A8301 (day 2)",
+      "APS-LM;C4A30F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "MPS-LM;B40C6A30F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "PPS-LM;B40C6F20 (day 1);B30+IWP2+A8301 (day 2)",
+      "PPS-DEb (mstar);B40C6F20 (day 1);A100L250 (day 2)",
+    ],
+    "C:/Users/zhiyu/OneDrive - Umich/dump/2025-05-08/scatter_FOXA2_MP",
+    "HAND1_FOXA2_",
+    "_scatter_density.png",
+    "matlabOrange"
   ),
 ];
 
-gridList[0].setChildGrid(gridList[1], "rightTop");
-gridList[0].setChildGrid(gridList[2], "bottomLeft");
+gridList[0].setChildGrid(gridList[1], "bottomLeft");
+gridList[0].setChildGrid(gridList[2], "rightTop");
+gridList[2].setChildGrid(gridList[3], "bottomLeft");
 
 const rootGrid = 0;
 
@@ -145,15 +198,15 @@ function getPageSize(grid) {
 
     const bd_width = Math.max(
       sz1[0] +
-        gap +
-        grid.ncol * subGridWidth +
-        (grid.ncol - 1) * gap +
-        sideBarWidth,
+      gap +
+      grid.ncol * subGridWidth +
+      (grid.ncol - 1) * gap +
+      sideBarWidth,
       sz2[0] +
-        gap +
-        grid.ncol * subGridWidth +
-        (grid.ncol - 1) * gap +
-        sideBarWidth,
+      gap +
+      grid.ncol * subGridWidth +
+      (grid.ncol - 1) * gap +
+      sideBarWidth,
       sz3[0] + sideBarWidth
     );
     const bd_height = Math.max(
@@ -216,6 +269,67 @@ myPage.rectangles.add({
   fillColor: "None",
 });
 
+
+// Add title text
+if (supTitleBarHeight > 0) {
+  var titleTextFrame = myPage.textFrames.add({
+    geometricBounds: [
+      margin,
+      margin,
+      margin + supTitleBarHeight,
+      margin + 500,
+    ],
+  });
+  titleTextFrame.contents = " ";
+  titleTextFrame.texts[0].appliedFont = myFont;
+  titleTextFrame.texts[0].fontStyle = "Bold";
+  titleTextFrame.texts[0].pointSize = titleFontSize;
+  titleTextFrame.textColumns.everyItem().fillColor = "black";
+  titleTextFrame.fit(FitOptions.FRAME_TO_CONTENT);
+}
+
+if (supSideBarWidth > 0) {
+  // Add channel names to the sidebar
+  var yPosition = margin + supTitleBarHeight + textGap / 2;
+  var textFrame = myPage.textFrames.add({
+    geometricBounds: [
+      yPosition - textGap / 2, // top
+      pageSize[0] + 20, // left
+      yPosition + textGap / 2, // bottom
+      pageSize[0] + supSideBarWidth, // right
+    ],
+  });
+  // Add the channel name text
+  // textFrame.contents = "channels:";
+  textFrame.contents = "duplicate 1";
+  // Set the text formatting
+  textFrame.texts[0].appliedFont = myFont;
+  textFrame.texts[0].fontStyle = "Bold";
+  textFrame.texts[0].pointSize = sideBarFontSize;
+  textFrame.textColumns.everyItem().fillColor = "matlabBlue";
+  // fit content to frame
+  textFrame.fit(FitOptions.FRAME_TO_CONTENT);
+
+  var yPosition = yPosition + textGap;
+  var textFrame = myPage.textFrames.add({
+    geometricBounds: [
+      yPosition - textGap / 2, // top
+      pageSize[0] + 20, // left
+      yPosition + textGap / 2, // bottom
+      pageSize[0] + supSideBarWidth, // right
+    ],
+  });
+  // Add the channel name text
+  // textFrame.contents = "channels:";
+  textFrame.contents = "duplicate 2";
+  // Set the text formatting
+  textFrame.texts[0].appliedFont = myFont;
+  textFrame.texts[0].fontStyle = "Bold";
+  textFrame.texts[0].pointSize = sideBarFontSize;
+  textFrame.textColumns.everyItem().fillColor = "matlabOrange";
+  // fit content to frame
+  textFrame.fit(FitOptions.FRAME_TO_CONTENT);
+}
 
 function drawGrid(grid) {
   if (grid == null) {
@@ -280,46 +394,64 @@ function drawGrid(grid) {
           strokeColor: "Black",
         });
 
-        try {
-          subGridRect.place(
-            File(
-              grid.folder_path +
-                "/" +
-                grid.prefix +
-                grid.condition_list[next_img_index] +
-                grid.postfix
-            )
-          );
-          next_img_index++;
-          subGridRect.fit(FitOptions.PROPORTIONALLY);
-          subGridRect.frameFittingOptions.fittingAlignment =
-            AnchorPoint.BOTTOM_CENTER_ANCHOR;
+        subGridRect.place(
+          File(
+            grid.folder_path +
+            "/" +
+            grid.prefix +
+            grid.condition_list[next_img_index] +
+            grid.postfix
+          )
+        );
+        next_img_index++;
+        subGridRect.fit(FitOptions.PROPORTIONALLY);
+        subGridRect.frameFittingOptions.fittingAlignment =
+          AnchorPoint.BOTTOM_CENTER_ANCHOR;
 
-          subGridRect.frameFittingOptions.topCrop = cropTop;
-          subGridRect.frameFittingOptions.bottomCrop = cropBottom;
-          subGridRect.frameFittingOptions.leftCrop = cropLeft;
-          subGridRect.frameFittingOptions.rightCrop = cropRight;
-        } catch (e) {
-          // alert("Error placing image: " + e);
-        }
+        subGridRect.frameFittingOptions.topCrop = cropTop;
+        subGridRect.frameFittingOptions.bottomCrop = cropBottom;
+        subGridRect.frameFittingOptions.leftCrop = cropLeft;
+        subGridRect.frameFittingOptions.rightCrop = cropRight;
+        // try {
+        //   subGridRect.place(
+        //     File(
+        //       grid.folder_path +
+        //       "/" +
+        //       grid.prefix +
+        //       grid.condition_list[next_img_index] +
+        //       grid.postfix
+        //     )
+        //   );
+        //   next_img_index++;
+        //   subGridRect.fit(FitOptions.PROPORTIONALLY);
+        //   subGridRect.frameFittingOptions.fittingAlignment =
+        //     AnchorPoint.BOTTOM_CENTER_ANCHOR;
+        //
+        //   subGridRect.frameFittingOptions.topCrop = cropTop;
+        //   subGridRect.frameFittingOptions.bottomCrop = cropBottom;
+        //   subGridRect.frameFittingOptions.leftCrop = cropLeft;
+        //   subGridRect.frameFittingOptions.rightCrop = cropRight;
+        // } catch (e) {
+        //   // alert("Error placing image: " + e);
+        // }
       }
       // add channel names
     }
     if (sideBarWidth > 0) {
-      var yPosition = gridOrigin_y + titleBarHeight + textGap / 2;
+      var yPosition = gridOrigin_y + titleBarHeight + textGap * 3 / 4;
       var textFrame = myPage.textFrames.add({
         geometricBounds: [
           yPosition - textGap / 2, // top
           gridOrigin_x +
-            grid.ncol * subGridWidth +
-            (grid.ncol - 1) * gap +
-            sideBarWidth * 0.05, // left
+          grid.ncol * subGridWidth +
+          (grid.ncol - 1) * gap +
+          sideBarWidth * 0.05, // left
           yPosition + textGap / 2, // bottom
           gridOrigin_x +
-            grid.ncol * subGridWidth +
-            (grid.ncol - 1) * gap +
-            sideBarWidth -
-            sideBarWidth * 0.05, // right
+          grid.ncol * subGridWidth +
+          (grid.ncol - 1) * gap +
+          sideBarWidth -
+          sideBarWidth * 0.05, // right
         ],
       });
       textFrame.contents = "channels:";
@@ -333,21 +465,21 @@ function drawGrid(grid) {
       for (var i = 0; i < grid.channelNames.length; i++) {
         // Calculate vertical position for each channel name
         var yPosition =
-          gridOrigin_y + titleBarHeight + (textGap / 2) * (1 + 2 * (i + 1));
+          gridOrigin_y + textGap / 3 + titleBarHeight + (textGap / 2) * (1 + 2 * (i + 1));
         // Create text frame in the sidebar
         var textFrame = myPage.textFrames.add({
           geometricBounds: [
             yPosition - textGap / 2, // top
             gridOrigin_x +
-              grid.ncol * subGridWidth +
-              (grid.ncol - 1) * gap +
-              sideBarWidth * 0.05, // left
+            grid.ncol * subGridWidth +
+            (grid.ncol - 1) * gap +
+            sideBarWidth * 0.05, // left
             yPosition + textGap / 2, // bottom
             gridOrigin_x +
-              grid.ncol * subGridWidth +
-              (grid.ncol - 1) * gap +
-              sideBarWidth -
-              sideBarWidth * 0.05, // right
+            grid.ncol * subGridWidth +
+            (grid.ncol - 1) * gap +
+            sideBarWidth -
+            sideBarWidth * 0.05, // right
           ],
         });
         // Add the channel name text
@@ -371,15 +503,15 @@ function drawGrid(grid) {
         geometricBounds: [
           yPosition - textGap / 2, // top
           gridOrigin_x +
-            grid.ncol * subGridWidth +
-            (grid.ncol - 1) * gap +
-            sideBarWidth * 0.05, // left
+          grid.ncol * subGridWidth +
+          (grid.ncol - 1) * gap +
+          sideBarWidth * 0.05, // left
           yPosition + textGap / 2, // bottom
           gridOrigin_x +
-            grid.ncol * subGridWidth +
-            (grid.ncol - 1) * gap +
-            sideBarWidth -
-            sideBarWidth * 0.05, // right
+          grid.ncol * subGridWidth +
+          (grid.ncol - 1) * gap +
+          sideBarWidth -
+          sideBarWidth * 0.05, // right
         ],
       });
       // Add the channel name text
@@ -400,15 +532,15 @@ function drawGrid(grid) {
         geometricBounds: [
           yPosition - textGap / 2, // top
           gridOrigin_x +
-            grid.ncol * subGridWidth +
-            (grid.ncol - 1) * gap +
-            sideBarWidth * 0.05, // left
+          grid.ncol * subGridWidth +
+          (grid.ncol - 1) * gap +
+          sideBarWidth * 0.05, // left
           yPosition + textGap / 2, // bottom
           gridOrigin_x +
-            grid.ncol * subGridWidth +
-            (grid.ncol - 1) * gap +
-            sideBarWidth -
-            sideBarWidth * 0.05, // right
+          grid.ncol * subGridWidth +
+          (grid.ncol - 1) * gap +
+          sideBarWidth -
+          sideBarWidth * 0.05, // right
         ],
       });
       // Add the channel name text
@@ -420,6 +552,22 @@ function drawGrid(grid) {
       textFrame.textColumns.everyItem().fillColor = "Black";
       // fit content to frame
       textFrame.fit(FitOptions.FRAME_TO_CONTENT);
+    }
+    if (titleBarHeight > 0) {
+      var titleTextFrame = myPage.textFrames.add({
+        geometricBounds: [
+          gridOrigin_y + subGridOffset,
+          gridOrigin_x + subGridOffset,
+          gridOrigin_y + titleBarHeight,
+          gridOrigin_x + gridWidth,
+        ],
+      });
+      titleTextFrame.contents = grid.title;
+      titleTextFrame.texts[0].appliedFont = myFont;
+      titleTextFrame.texts[0].fontStyle = "Bold";
+      titleTextFrame.texts[0].pointSize = titleFontSize;
+      titleTextFrame.textColumns.everyItem().fillColor = "black";
+      titleTextFrame.fit(FitOptions.FRAME_TO_CONTENT);
     }
   }
 }
